101b6e36da68d3eeb0855747ca7c2c6fc0c8c79e
Sebastian Largacha <chitas1004@hotmail.com>
2024-07-03 00:17:18 -0500

    Mi primer commit

---
+++ src/main/java/co/com/cajarcol/literjacol/LiterjacolApplication.java
@@ -0,0 +1,13 @@
+package co.com.cajarcol.literjacol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterjacolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterjacolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/cajarcol/literjacol/LiterjacolApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.cajarcol.literjacol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterjacolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
